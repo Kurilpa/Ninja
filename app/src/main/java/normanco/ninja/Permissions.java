c335e57afac752d21b2a0344305af3f5ffc118cd
Tom <39142955+Kurilpa@users.noreply.github.com>
2018-06-18 18:00:56 +1000

    Added camera permissions
    Add method to get the camera permissions

--- app/src/main/java/normanco/ninja/Permissions.java
+++ app/src/main/java/normanco/ninja/Permissions.java
@@ -17,13 +17,14 @@ import android.support.v4.content.ContextCompat;
 public class Permissions {
 
     public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+    public static final int PERMISSION_MULTI_REQUEST = 789;
 
     public static boolean checkPermissionREAD_EXTERNAL_STORAGE(final Context context) {
         int currentAPIVersion = Build.VERSION.SDK_INT;
         if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE);
+                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
                 }
                 else {
                     ActivityCompat.requestPermissions((Activity) context, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
@@ -40,18 +41,37 @@ public class Permissions {
         }
     }
 
-    public static void showDialog(final String msg, final Context context,
-                           final String permission) {
+    public static void showDialog(final String msg, final Context context, final String permission, final int type) {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
         alertBuilder.setCancelable(true);
         alertBuilder.setTitle("Permission necessary");
         alertBuilder.setMessage(msg + " permission is necessary");
         alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions((Activity) context, new String[] {permission}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                        ActivityCompat.requestPermissions((Activity) context, new String[] {permission}, type);
                     }
                 });
         AlertDialog alert = alertBuilder.create();
         alert.show();
+    }
+
+    public static boolean checkPermissionCAMERA(Context mContext) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA) + ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) mContext, Manifest.permission.CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale((Activity) mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    showDialog("Add permissions to take photo", mContext, Manifest.permission.READ_EXTERNAL_STORAGE, PERMISSION_MULTI_REQUEST);
+                } else {
+                    ActivityCompat.requestPermissions((Activity) mContext, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_MULTI_REQUEST);
+                }
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else{
+            return true;
+        }
     }
 }
