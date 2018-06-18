ba42eb236cca85c37bbbdf0e05fbe0391700439b
Tom <39142955+Kurilpa@users.noreply.github.com>
2018-06-18 18:01:31 +1000

    Add first start flag

--- app/src/main/java/normanco/ninja/SharedData.java
+++ app/src/main/java/normanco/ninja/SharedData.java
@@ -21,11 +21,15 @@ public class SharedData {
         editor = sharedPref.edit();
     }
 
+    public static void setImagePath(String string) {
+        editor.putString("image", string);
+    }
+
     public static void setImagePath(Context mContext, Uri uri){
         GetPathFromUri getPathFromUri = new GetPathFromUri();
         String path = getPathFromUri.getUriRealPath(mContext, uri);
         editor.putString("image", path);
-        Log.i(TAG, "Shared Pref saved image path: " + path);
+        Log.i(TAG, "Shared Pref saved imageBackground path: " + path);
     }
 
     public static void setDueDate(int dateDue){
@@ -38,16 +42,17 @@ public class SharedData {
         editor.putInt("dateBorn", dateBorn);
     }
 
-    public static boolean getDataFlag(){
+    public static void setFirstFlag(){
+        editor.putBoolean("firstStart", false);
+    }
+
+    public static boolean getFirstFlag(){
         //looks for the Shared Preference flags due_date & born_date to see if the dates have been assigned;
-        int dateDue = sharedPref.getInt("dateDue", 0);
-        int dateBorn = sharedPref.getInt("dateBorn", 0);
-        return (!(dateDue == 0 || dateBorn == 0));
+        return sharedPref.getBoolean("firstStart", true);
     }
 
     public static String getImagePath(){
-        Log.i(TAG, "Shared Pref retrieved image path: " + sharedPref.getString("image", "false"));
-        return sharedPref.getString("image", "false");
+        return sharedPref.getString("image", "default");
     }
 
     public static int getDueDate(){
