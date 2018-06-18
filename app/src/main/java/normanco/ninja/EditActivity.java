24dcee754d0e0f44c05a4a2345cc1b7da5920a16
Tom <39142955+Kurilpa@users.noreply.github.com>
2018-06-18 17:03:47 +1000

    Camera support and Permission handeling
    Changed the class so that the camera is now invoked when the camera button is pressed. Permissions are now requested when they are required not at the start of the app. Various other changes to handle images better.

--- app/src/main/java/normanco/ninja/EditActivity.java
+++ app/src/main/java/normanco/ninja/EditActivity.java
@@ -1,13 +1,17 @@
 package normanco.ninja;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.TransitionManager;
 import android.util.Log;
@@ -18,7 +22,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class EditActivity extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
     private static String TAG = "LogMessage";
@@ -30,16 +38,20 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
     ImageButton btnGallery;
     ImageButton btnCamera;
     ImageButton btnAccept;
+    ImageButton btnDefault;
     TextView txvDueDate;
     TextView txvBornDate;
-    ImageView imvBgEdit;
-    Uri image = null;
+    ImageView imvBackground;
+    Image image;
+    Uri imageBackground;
+    String mCurrentPhotoPath;
     boolean collapsed = true;               //stores the state of the button stack.
     int dateDue;                            //stores the due date entered
     int dateBorn;                           //stores the birth date entered
     int viewClicked;                        //stores which date was selected to be edited
-    int usingDefImage = Image.DEFAULT_IMAGE;
+    int usingDefImage;
     private static final int PICK_IMAGE = 100;
+    private static final int CAMERA_REQUEST = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +68,7 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
         btnAccept = findViewById(R.id.btn_ok);
         txvDueDate = findViewById(R.id.txv_due_date);
         txvBornDate = findViewById(R.id.txv_birth_date);
-        imvBgEdit = findViewById(R.id.imv_photo_edit);
+        imvBackground = findViewById(R.id.imv_photo_edit);
 
         //set the defaults;
         setDefaults(savedInstanceState);
@@ -91,22 +103,35 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(Permissions.checkPermissionREAD_EXTERNAL_STORAGE(mContext))
                 openGallery();
             }
         });
 
-        //Camera button on click listen. usding to reset shared preferences
+        //Camera button on click listener
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Image.setDefImageUri();
-                image = Image.getDefImageUri();
-                Matrix matrix = Image.rotateImage(mContext, image, Image.DEFAULT_IMAGE);
-                imvBgEdit.setScaleType(ImageView.ScaleType.MATRIX);
-                imvBgEdit.setImageMatrix(matrix);
-                imvBgEdit.setImageURI(image);
+                if(Permissions.checkPermissionCAMERA(mContext)){
+                    openCamera();
+                }
             }
         });
+
+        //Default image button on click listen.
+        //btnDefault.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        usingDefImage = Image.DEFAULT_IMAGE;
+        //        imageBackground = image.getDefImageUri();
+        //       Matrix matrix = Image.rotateImage(mContext, imageBackground, usingDefImage);
+        //        imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+        //        imvBackground.setImageMatrix(matrix);
+        //        imvBackground.setImageURI(imageBackground);
+        //        SharedData.setImagePath("default");
+        //        SharedData.applyData();
+        //    }
+        //});
 
         // Ok button onclick listener
         btnAccept.setOnClickListener(new View.OnClickListener() {
@@ -115,15 +140,19 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
                 SharedData.setSharedPref(getApplicationContext());
                 SharedData.setBornDate(dateBorn);
                 SharedData.setDueDate(dateDue);
-                SharedData.setImagePath(mContext, image);
+                SharedData.setFirstFlag();
+                if(usingDefImage == 0){
+                    SharedData.setImagePath(mContext, imageBackground);
+                }
+
                 SharedData.applyData();
 
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("image", image);
+                intent.putExtra("image", imageBackground);
                 intent.putExtra("flag", 1);
                 intent.putExtra("usingDefImage", usingDefImage);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                Log.i(TAG, "Edit Activity passed image URI: " + image);
+                Log.i(TAG, "Edit Activity passed imageBackground URI: " + imageBackground);
                 finish();
                 startActivity(intent);
             }
@@ -135,7 +164,7 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
         super.onSaveInstanceState(outState);
         outState.putInt("dateDue", dateDue);
         outState.putInt("dateBorn", dateBorn);
-        outState.putParcelable("image", image);
+        outState.putParcelable("image", imageBackground);
         outState.putInt("usingDefImage", usingDefImage);
     }
 
@@ -159,13 +188,31 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
         super.onActivityResult(requestCode, resultCode, data);
 
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-            image = data.getData();
+            imageBackground = data.getData();
             usingDefImage = 0;
-            Matrix matrix = Image.rotateImage(this, image, usingDefImage);
-            imvBgEdit.setScaleType(ImageView.ScaleType.MATRIX);
-            imvBgEdit.setImageMatrix(matrix);
-            imvBgEdit.setImageURI(image);
+            Matrix matrix = Image.rotateImage(this, imageBackground, usingDefImage);
+            imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+            imvBackground.setImageMatrix(matrix);
+            imvBackground.setImageURI(imageBackground);
         }
+
+        if(resultCode == Activity.RESULT_OK && requestCode == CAMERA_REQUEST) {
+            Log.i(TAG, "In Camera on action");
+            usingDefImage = 0;
+            image = new Image(this, mCurrentPhotoPath);
+            imageBackground = image.getImageUri();
+            Log.i(TAG, "Parse photo Uri:" + imageBackground);
+            Matrix matrix = Image.rotateImage(this, imageBackground, usingDefImage);
+            imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+            imvBackground.setImageMatrix(matrix);
+            imvBackground.setImageURI(imageBackground);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        openGallery();
     }
 
     private void buttonCollapse(Context mContext) {
@@ -187,54 +234,98 @@ public class EditActivity extends AppCompatActivity implements DatePickerFragmen
         if(savedInstanceState != null){
             dateDue = savedInstanceState.getInt("dateDue");
             dateBorn = savedInstanceState.getInt("dateBorn");
-            image = savedInstanceState.getParcelable("image");
+            imageBackground = savedInstanceState.getParcelable("image");
             usingDefImage = savedInstanceState.getInt("usingDefImage");
             txvDueDate.setText(DatesToWords.parseDateToWord(dateDue));
             txvBornDate.setText(DatesToWords.parseDateToWord(dateBorn));
-            Matrix matrix = Image.rotateImage(this, image, usingDefImage);
-            imvBgEdit.setScaleType(ImageView.ScaleType.MATRIX);
-            imvBgEdit.setImageMatrix(matrix);
-            imvBgEdit.setImageURI(image);
+            Matrix matrix = Image.rotateImage(this, imageBackground, usingDefImage);
+            imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+            imvBackground.setImageMatrix(matrix);
+            imvBackground.setImageURI(imageBackground);
 
-            Log.i(TAG, "Image Uri 1: " + image);
+            Log.i(TAG, "Image Uri 1: " + imageBackground);
         }
-        else if (SharedData.getDataFlag()){
-            Intent intent = getIntent();
+        else if (!SharedData.getFirstFlag()){
             dateDue = SharedData.getDueDate();
             dateBorn = SharedData.getBornDate();
-            image = intent.getParcelableExtra("image");
+
+            if(SharedData.getImagePath().equals("default")) {
+                Log.i(TAG, "Edit Activity - Setting image to default");
+                image = new Image();
+                imageBackground = image.getDefImageUri();
+                usingDefImage = Image.DEFAULT_IMAGE;
+            }
+            else {
+                Log.i(TAG, "Edit Activity - Setting image to user");
+                image = new Image(this, SharedData.getImagePath());
+                imageBackground = image.getImageUri();
+                usingDefImage = 0;
+            }
+
             txvDueDate.setText(DatesToWords.parseDateToWord(dateDue));
             txvBornDate.setText(DatesToWords.parseDateToWord(dateBorn));
-            Matrix matrix = Image.rotateImage(this, image, usingDefImage);
-            imvBgEdit.setScaleType(ImageView.ScaleType.MATRIX);
-            imvBgEdit.setImageMatrix(matrix);
-            imvBgEdit.setImageURI(image);
 
-            Log.i(TAG, "Image Uri 2: " + image);
+            Matrix matrix = Image.rotateImage(this, imageBackground, usingDefImage);
+            imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+            imvBackground.setImageMatrix(matrix);
+            imvBackground.setImageURI(imageBackground);
+
+            Log.i(TAG, "Image Uri 2: " + imageBackground);
         }
         else {
+            image = new Image();
+            Matrix matrix = Image.rotateImage(this, imageBackground, Image.DEFAULT_IMAGE);
             Calendar calendar = Calendar.getInstance();
             int year = calendar.get(Calendar.YEAR);
             int month = calendar.get(Calendar.MONTH);
             int day = calendar.get(Calendar.DAY_OF_MONTH);
-            Intent intent = getIntent();
-            dateDue = dateBorn = year * 10000 + month * 100 + day;
-            image = intent.getParcelableExtra("image"); Log.i(TAG, "Received Image");
 
+            dateDue = dateBorn = year * 10000 + month * 100 + day;
+            imageBackground = image.getDefImageUri();
             txvDueDate.setText(DatesToWords.parseDateToWord(dateDue));
             txvBornDate.setText(DatesToWords.parseDateToWord(dateBorn));
-            Matrix matrix = Image.rotateImage(this, image, usingDefImage);
-            imvBgEdit.setScaleType(ImageView.ScaleType.MATRIX);
-            imvBgEdit.setImageMatrix(matrix);
-            imvBgEdit.setImageURI(image);
+            imvBackground.setScaleType(ImageView.ScaleType.MATRIX);
+            imvBackground.setImageMatrix(matrix);
+            imvBackground.setImageURI(imageBackground);
 
-            Log.i(TAG, "Image Uri 3: " + image);
+            Log.i(TAG, "Image Uri 3: " + imageBackground);
         }
     }
 
     private void openGallery(){
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, PICK_IMAGE);
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch(IOException e) {
+                Log.i(TAG, "Camera file creation error: " + e);
+            }
+
+            if(photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "normanco.ninja.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+                Log.i(TAG, "Photo Path: " + photoFile);    Log.i(TAG, "Photo Uri: " + photoUri);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imagePath = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = imagePath.getAbsolutePath();
+
+        return imagePath;
     }
 
 }
